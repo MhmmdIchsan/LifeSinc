49ff39580f4c79245caedead6c3e9168b8bfb747
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-05-25 16:08:30 +0700

    feat: Add Goals model and migration

---
+++ app/Enums/Goals.ts
@@ -0,0 +1,6 @@
+enum Goals {
+    NORMAL = 1,
+    DIET = 2
+}
+
+export default Goals
---
+++ app/models/goal.ts
@@ -0,0 +1,27 @@
+import { DateTime } from 'luxon'
+import User from './user.js'
+import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
+import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
+
+export default class Goal extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @hasMany(() => User)
+  declare users: HasMany<typeof User>
+
+  @column()
+  declare tracking: number
+
+  @belongsTo(() => Goal)
+  declare goal: BelongsTo<typeof Goal>
+}
