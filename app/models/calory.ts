4c493b0e75fc13f695b0668b4fa31c542d8a9ff5
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-05-28 03:08:05 +0700

    feat: Add amount and date columns to Calory model and migration

--- app/models/calory.ts
+++ app/models/calory.ts
@@ -1,7 +1,11 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import Food from '#models/food'
+import Meal from '#models/meal'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 
 export default class Calory extends BaseModel {
+  [x: string]: any
   @column({ isPrimary: true })
   declare id: number
 
@@ -11,12 +15,30 @@ export default class Calory extends BaseModel {
   @column()
   declare foodsId: number
 
+  @belongsTo(() => Food, {
+    foreignKey: 'foodsId',
+  })
+  declare food: BelongsTo<typeof Food>
+
   @column()
   declare mealId: number
+
+  @belongsTo(() => Meal, {
+    foreignKey: 'mealId',
+  })
+  declare meal: BelongsTo<typeof Meal>
+
+  @column()
+  declare amount: number
+
+  @column.date()
+  declare date: DateTime
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+
 }
